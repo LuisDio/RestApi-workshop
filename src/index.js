95fa23f06cf875d135c00a8a10be1fb3c555eaa4
Luis <luis.diolindo@gmail.com>
2019-09-05 23:16:09 +0300

    commenting

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ let express = require('express')
 
 let app = express()
 
+// import route from route folder
 let personRoute = require('./routes/person')
 
+// using route to person
 app.use(personRoute)
 
 // Serving static file 
