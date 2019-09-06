a09133d6fc0a3d2c40f4a05f185d2bacb14f2b7e
Luis <luis.diolindo@gmail.com>
2019-09-07 00:03:51 +0300

    404 handler and middleware

--- src/index.js
+++ src/index.js
@@ -5,13 +5,20 @@ let app = express()
 // import route from route folder
 let personRoute = require('./routes/person')
 
+app.use((req, res, next) => {
+    console.log(`${new Date().toString()} => ${req.originalUrl}`)
+    next()
+})
 // using route to person
 app.use(personRoute)
 
 // Serving static file 
 app.use(express.static('public'))
 
-
+// Handler for 404
+app.use((req, res, next) => {
+    res.status(404).send('We think you are lost')
+})
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.info(`Server has started on ${PORT}`));
