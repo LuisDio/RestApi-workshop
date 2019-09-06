2614488744701e395c9145bead230580d96fcf25
Luis <luis.diolindo@gmail.com>
2019-09-07 00:19:37 +0300

    500 error handler

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ let express = require('express')
 
 let app = express()
 
+let path = require('path')
+
 // import route from route folder
 let personRoute = require('./routes/person')
 
@@ -15,9 +17,16 @@ app.use(personRoute)
 // Serving static file 
 app.use(express.static('public'))
 
-// Handler for 404
+// Handler for 404 - Ressource not found
 app.use((req, res, next) => {
     res.status(404).send('We think you are lost')
+})
+
+// Handle for Error 500
+app.use((err, req, res, next) => {
+    console.log(err)
+
+    res.sendFile(path.join(__dirname, '../public/500.html'))
 })
 
 const PORT = process.env.PORT || 3000
--- src/routes/person.js
+++ src/routes/person.js
@@ -18,4 +18,8 @@ router.get('/person/:name', (req, res) => {
     res.send(`You have requested a person ${req.params.name}`)
 });
 
+router.get('/error', (req, res) => {
+    throw new Error('This is a forced error.')
+})
+
 module.exports = router
